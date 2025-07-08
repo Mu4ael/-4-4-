58ca6d59075420678e47661a78bf202d22c16c98
Mu4ael <net393791@gmail.com>
2025-07-08 20:14:05 +0600

    first

--- main.js
+++ main.js
@@ -11,29 +11,25 @@ closeCart.addEventListener("click", () => {
     cart.classList.remove("open");
 });
 
-
 fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
-    .then((data) => {
-        renderCards(data);
+    .then((products) => {
+        list.innerHTML = "";
+
+        products.forEach((el) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            card.innerHTML = `
+                <div class="image">
+                    <img src="${el.image}" alt="${el.title}">
+                </div>
+                <h2>${el.title}</h2>
+                <p>${el.price}$</p>
+                <button class="buttonCart">Add To Cart</button>
+            `;
+
+            list.appendChild(card);
+        });
     });
 
-function renderCards(products) {
-    list.innerHTML = "";
-
-    products.forEach((el) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        card.innerHTML = `
-            <div class="image">
-                <img src="${el.image}" alt="${el.title}">
-            </div>
-            <h2>${el.title}</h2>
-            <p>${el.price}$</p>
-            <button class="buttonCart">Add To Cart</button>
-        `;
-
-        list.appendChild(card);
-    });
-}
